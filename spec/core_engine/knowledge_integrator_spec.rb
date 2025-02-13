ffb01abe24854de97e9ff0354e3c5cf5f9532d21
Ghassan Alhamoud <ghassan.alhamoud@vinted.com>
2025-02-13 15:08:02 +0100

    update KnowledgeIntegrator

--- spec/core_engine/knowledge_integrator_spec.rb
+++ spec/core_engine/knowledge_integrator_spec.rb
@@ -1,25 +1,19 @@
 require 'spec_helper'
 
 RSpec.describe ResearchAssistant::CoreEngine::KnowledgeIntegrator do
-  let(:integrator) { described_class.new }
   let(:mock_analyzer) { instance_double('ResponseAnalyzer') }
   let(:mock_updater) { instance_double('ConceptUpdater') }
   let(:mock_detector) { instance_double('GapDetector') }
-
-  before do
-    allow(ResponseAnalyzer).to receive(:new).and_return(mock_analyzer)
-    allow(ConceptUpdater).to receive(:new).and_return(mock_updater)
-    allow(GapDetector).to receive(:new).and_return(mock_detector)
-  end
+  let(:integrator) { described_class.new(response_analyzer: mock_analyzer, concept_updater: mock_updater, gap_detector: mock_detector) }
 
   describe '#integrate' do
     it 'processes all responses' do
       analysis = { insights: [], core_concepts: [], knowledge_gaps: [] }
       responses = ["Response 1", "Response 2"]
 
-      expect(mock_analyzer).to receive(:analyze).twice
-      expect(mock_updater).to receive(:update).twice
-      expect(mock_detector).to receive(:detect).twice
+      expect(mock_analyzer).to receive(:analyze).twice.and_return(analysis)
+      expect(mock_updater).to receive(:update).twice.and_return(analysis)
+      expect(mock_detector).to receive(:detect).twice.and_return(analysis)
 
       integrator.integrate(analysis, responses)
     end
