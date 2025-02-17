4dbb60bd3f791684fff1a66ea7670041dcce8951
Ghassan Alhamoud <ghassan.alhamoud@vinted.com>
2025-02-17 16:44:55 +0100

    impl Relations Finder

---
+++ lib/research_assistant/core_engine/relations_finder.rb
@@ -0,0 +1,27 @@
+require_relative 'models/response_schema_prompt'
+
+module ResearchAssistant
+  module CoreEngine
+    class RelationsFinder
+
+      attr_reader :api_client, :json_api_client
+
+      def initialize(api_client, json_api_client)
+        @api_client = api_client
+        @json_api_client = json_api_client
+      end
+
+      def find_relations(text, analysis)
+        prompt = "Please analyze the given text to identify relationships between concepts: #{text}, Analysis: #{analysis}"
+        response = api_client.query(prompt)
+        parse_response(response)
+      end
+
+      private
+
+      def parse_response(response)
+        json_api_client.query(response, Models::RELATIONS_SCHEMA)
+      end
+    end
+  end
+end
---
+++ spec/core_engine/relations_finder_spec.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe ResearchAssistant::CoreEngine::RelationsFinder do
+  let(:api_client) { instance_double(ResearchAssistant::OllamaInterface::ApiClient) }
+  let(:json_api_client) { instance_double(ResearchAssistant::OllamaInterface::JsonApiClient) }
+  let(:finder) { described_class.new(api_client, json_api_client) }
+  let(:text) { 'The sky is blue and the sun is bright.' }
+  let(:analysis) { { insights: [], core_concepts: [], knowledge_gaps: [] } }
+  let(:api_response) {
+    [
+      { 'insight' => 'The sky is blue.', 'classification' => 'associative', 'significance' => 'It describes the color of the sky.' },
+      { 'insight' => 'The sun is bright.', 'classification' => 'causal', 'significance' => 'It describes the brightness of the sun.' }
+    ]
+  }
+
+  describe '#find_relations' do
+    context 'when the API request is successful' do
+      it 'returns the identified relationships' do
+        allow(api_client).to receive(:query).and_return(api_response.to_json)
+        allow(json_api_client).to receive(:query).with(api_response.to_json, ResearchAssistant::CoreEngine::Models::RELATIONS_SCHEMA).and_return(api_response)
+
+        relations = finder.find_relations(text, analysis)
+        expect(relations).to be_an(Array)
+        expect(relations).not_to be_empty
+        expect(relations).to all(include('insight', 'classification', 'significance'))
+      end
+    end
+
+    context 'when the API request fails' do
+      it 'raises an API error' do
+        allow(api_client).to receive(:query).and_raise(RuntimeError, 'API Error: Something went wrong')
+
+        expect { finder.find_relations(text, analysis) }.to raise_error(RuntimeError, 'API Error: Something went wrong')
+      end
+    end
+
+    context 'when there is a connection error' do
+      it 'raises a connection error' do
+        allow(api_client).to receive(:query).and_raise(RuntimeError, 'Connection Error: Connection failed')
+
+        expect { finder.find_relations(text, analysis) }.to raise_error(RuntimeError, 'Connection Error: Connection failed')
+      end
+    end
+  end
+end
