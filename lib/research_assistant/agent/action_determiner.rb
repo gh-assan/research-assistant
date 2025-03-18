81f15669f8a052728ff01b08c02a224d35e303b5
Ghassan Alhamoud <ghassan.alhamoud@vinted.com>
2025-03-18 18:20:30 +0100

    remove the comments

--- lib/research_assistant/agent/action_determiner.rb
+++ lib/research_assistant/agent/action_determiner.rb
@@ -16,16 +16,13 @@ module ResearchAssistant
       end
 
       def get_next_action(article)
-        # Combine the article and action history
         input = {
           article: article,
           action_history: action_history
         }
 
-        # Query the client with the combined input
         response = json_api_client.query(input.to_s, SCHEME)
 
-        # Create the action and update the history
         action = Action.new(name: response['action'], reasons: response['reasons'])
         action_history << action.name
 
